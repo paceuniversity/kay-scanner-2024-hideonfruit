bf1b31ecf3f0bf17c5b944b3e38b81a36b750604
hideonfruit <148816660+hideonfruit@users.noreply.github.com>
2024-12-15 02:31:07 -0500

    Update ScannerDemo.java

--- ScannerDemo.java
+++ ScannerDemo.java
@@ -1,17 +1,23 @@
 /**
- * @author Sandeep and Karan
+ * @author scharffc
+ * 
  */
 public class ScannerDemo {
 
-    private static String file1 = "C:\\Users\\sande\\Desktop\\prog1.kay";
-    private static int counter = 1;
+	// This needs to be changed !
+	private static String file1 = "prog1.kay";
+	private static int counter = 1;
 
-    public static void main(String args[]) {
-        TokenStream ts = new TokenStream(file1);
-        System.out.println(file1);
-        while (!ts.isEndofFile()) {
-            Token tk = ts.nextToken();
-            System.out.println("Token " + counter++ + " - Type: " + tk.getType() + " - Value: " + tk.getValue());
-        }
-    }
+	public static void main(String args[]) {
+		TokenStream ts = new TokenStream(file1);
+		System.out.println(file1);
+		while (!ts.isEndofFile()) {
+			// TO BE COMPLETED
+			Token tk = ts.nextToken();
+			if (tk == null) {
+    		break; // End of file reached
+    		}
+			System.out.println("Token " + counter++ + " - Type: " + tk.getType() + " - Value: " + tk.getValue());
+		}
+	}
 }
